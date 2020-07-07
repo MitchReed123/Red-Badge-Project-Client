4da1b0bfd4018501b64fadd070edde89c710966f
mitch reed <reed.mitch@live.com>
2020-07-07 15:27:17 -0400

    client side update

--- src/components/lottoCRUD/lottoEdit.tsx
+++ src/components/lottoCRUD/lottoEdit.tsx
@@ -1,11 +1,102 @@
 // grabbing from Lotto Table, http://localhost:3000/:id, PUT(UPDATE)
 import React from "react";
-export default class LottoEdit extends React.Component {
+import {
+  FormGroup,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  Label,
+  Input,
+  Button,
+} from "reactstrap";
+
+type acceptedProps = {
+  updateOff: any;
+  token: any;
+  fetchLottos: any;
+  setUpdateLotto: any;
+};
+
+type valueTypes = {
+  editLottoName: string | any;
+  editLottoPot: string | any;
+  editLottoLoco: string | any;
+};
+
+export default class LottoEdit extends React.Component<
+  acceptedProps,
+  valueTypes
+> {
+  constructor(props: acceptedProps) {
+    super(props);
+    this.state = {
+      editLottoName: this.props.setUpdateLotto.nameOfLotto,
+      editLottoPot: this.props.setUpdateLotto.lottoPot,
+      editLottoLoco: this.props.setUpdateLotto.location,
+    };
+  }
+
+  handleSubmit = (event: any) => {
+    event.preventDefault();
+    fetch(`http://localhost:3000/lotto/${this.props.setUpdateLotto.id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        lottery: {
+          nameOfLotto: this.state.editLottoName,
+          lottoPot: this.state.editLottoPot,
+          location: this.state.editLottoLoco,
+        },
+      }),
+      headers: new Headers({
+        "Content-type": "application/json",
+        Authorization: this.props.token,
+      }),
+    }).then((res) => {
+      this.props.fetchLottos();
+      this.props.updateOff();
+    });
+  };
   render() {
     return (
-      <div>
-        <h1>Testing</h1>
-      </div>
+      <Modal isOpen={true}>
+        <ModalHeader>Edit a Lotto</ModalHeader>
+        <ModalBody>
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label htmlFor="nameOfLotto">Edit a Lotto Name</Label>
+              <Input
+                name="nameOfLotto"
+                value={this.state.editLottoName}
+                onChange={(e) =>
+                  this.setState({ editLottoName: e.target.value })
+                }
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="lottoPot">Edit a Lotto Pot</Label>
+              <Input
+                name="lottoPot"
+                value={this.state.editLottoPot}
+                onChange={(e) =>
+                  this.setState({ editLottoPot: e.target.value })
+                }
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="location">Edit Lotto Location</Label>
+              <Input
+                name="location"
+                value={this.state.editLottoLoco}
+                onChange={(e) =>
+                  this.setState({ editLottoLoco: e.target.value })
+                }
+              />
+            </FormGroup>
+            <Button type="submit">Update Lotto</Button>
+          </Form>
+        </ModalBody>
+      </Modal>
     );
   }
 }
--- src/components/lottoCRUD/lottoIndex.tsx
+++ src/components/lottoCRUD/lottoIndex.tsx
@@ -3,16 +3,22 @@ import React, { Component, useState, useEffect } from "react";
 import LottoCreate from "./lottoCreate";
 import LottoTable from "./lottoTable";
 import LottoEdit from "./lottoEdit";
-// import {Container, Row, Col} from 'reactstrap';
+import { Container, Row, Col, Table, Button } from "reactstrap";
 type acceptedProps = {
   token: string | any;
 };
 type valueTypes = {
-  lottoNum: string;
+  lottoNum: number;
   nameOfLotto: string;
   lottoPot: string;
-  location: string;
+  location: [];
   dataTable: [];
+  setUpdateLotto: {};
+  setUpdateActive: boolean;
+  random: string;
+  locodatatable: [];
+  lottoEmpty: any;
+  hyphen: string;
 };
 export default class LottoIndex extends React.Component<
   acceptedProps,
@@ -21,42 +27,179 @@ export default class LottoIndex extends React.Component<
   constructor(props: acceptedProps) {
     super(props);
     this.state = {
-      lottoNum: "",
+      lottoNum: 0,
       nameOfLotto: "",
       lottoPot: "",
-      location: "",
+      location: [],
       dataTable: [],
+      setUpdateLotto: {},
+      locodatatable: [],
+      setUpdateActive: false,
+      random: "",
+      lottoEmpty: undefined,
+      hyphen: "-",
       //owner_id: '',
       //userId: '', //???
     };
   }
+
+  editUpdateLotto = (lotto: any) => {
+    console.log(lotto);
+    this.setState({ setUpdateLotto: lotto });
+  };
+
+  updateOn = () => {
+    this.setState({ setUpdateActive: true });
+  };
+
+  updateOff = () => {
+    this.setState({ setUpdateActive: false });
+  };
+
+  fetchLottos = () => {
+    fetch("http://localhost:3000/lotto/", {
+      method: "GET",
+      headers: {
+        "content-Type": "application/json",
+        Authorization: this.props.token,
+      },
+    })
+      .then((res) => res.json())
+      .then((lottoData) => {
+        console.log("Lotto Data", lottoData.lotteries);
+        this.setState({
+          dataTable: lottoData.lotteries,
+        });
+        console.log("Lottery State Variable", this.state.dataTable);
+      });
+  };
+  //this needs to be fixed, it wont work at all
+  deleteLotto = (lotto: any) => {
+    fetch(`http://localhost:3000/lotto/${lotto.id}`, {
+      method: "DELETE",
+      headers: new Headers({
+        "Content-type": "application/json",
+        Authorization: this.props.token,
+      }),
+    }).then(() => this.fetchLottos());
+  };
+
+  min = 1;
+  max = 49;
+
+  // addZero() {
+  //   return (this.state.random < "10" ? "0" : "") + this.state.random;
+  // }
+
+  randomNum = () => {
+    return (
+      Math.floor(1 + Math.random() * 45) +
+      "-" +
+      Math.floor(1 + Math.random() * 45) +
+      "-" +
+      Math.floor(1 + Math.random() * 45) +
+      "-" +
+      Math.floor(1 + Math.random() * 45) +
+      "-" +
+      Math.floor(1 + Math.random() * 45) +
+      "-" +
+      Math.floor(1 + Math.random() * 45)
+    );
+  };
+
+  handleClick = () => {
+    this.setState({
+      random:
+        // Math.floor(1 + Math.random() * 45) +
+        // "-" +
+        // Math.floor(1 + Math.random() * 45) +
+        // "-" +
+        // Math.floor(1 + Math.random() * 45) +
+        // "-" +
+        // Math.floor(1 + Math.random() * 45) +
+        // "-" +
+        // Math.floor(1 + Math.random() * 45) +
+        // "-" +
+        // Math.floor(1 + Math.random() * 45),
+        this.randomNum(),
+    });
+  };
+
+  lottoMapper = () => {
+    return this.state.dataTable.map((lotto: any, index) => {
+      return (
+        <tr key={index}>
+          <td>{lotto.lottoNum}</td>
+          <td>{lotto.nameOfLotto}</td>
+          <td>{lotto.lottoPot}</td>
+          <td>{lotto.location}</td>
+          <td>
+            <Button
+              onClick={() => {
+                this.editUpdateLotto(lotto);
+                this.updateOn();
+              }}
+            >
+              Update
+            </Button>
+          </td>
+          <td>
+            <Button
+              onClick={() => {
+                this.deleteLotto(lotto);
+              }}
+            >
+              Delete
+            </Button>
+          </td>
+        </tr>
+      );
+    });
+  };
   componentWillMount() {
     console.log("will mount");
   }
   componentDidMount() {
-    fetch("http://localhost:3000/lotto/", {
-      method: "GET",
-      headers: new Headers({
-        "Content-Type": "application/json",
-        Authorization: this.props.token,
-      }),
-    })
-      .then((res) => res.json())
-      .then((lottoData) => {
-        console.log(lottoData);
-        this.setState({
-          dataTable: lottoData,
-        });
-        console.log(this.state.dataTable); //able to set 'data' for all columns names?
-      });
+    this.fetchLottos();
+    this.lottoMapper();
   }
   render() {
     return (
       <div>
         <h1>Lotto index</h1>
-        <LottoCreate />
-        <LottoTable />
-        <LottoEdit />
+        <LottoCreate
+          token={this.props.token}
+          fetchLottos={this.fetchLottos}
+          // destinations={this.fetchLocos}
+        />
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.random}</h1>
+        <Row>
+          <Col>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Lotto Number</th>
+                  <th>Name of Lottery</th>
+                  <th>Lottery Pot</th>
+                  <th>Lotto location</th>
+                </tr>
+              </thead>
+              <tbody>{this.lottoMapper()}</tbody>
+            </Table>
+          </Col>
+        </Row>
+
+        {this.state.setUpdateActive ? (
+          <LottoEdit
+            updateOff={this.updateOff}
+            token={this.props.token}
+            fetchLottos={this.fetchLottos}
+            setUpdateLotto={this.state.setUpdateLotto}
+          />
+        ) : (
+          <></>
+        )}
       </div>
     );
   }
